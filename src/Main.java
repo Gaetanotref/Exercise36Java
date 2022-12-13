5f38bd93d460dfd372471fe29e4471016465e279
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-13 10:00:57 +0100

    Commit init

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String string1 = "x3z ? xYz ! R1 && __";
+        String string2 = string1.replaceAll("[a-z||0-9]", "*");
+        System.out.println(string2);
+    }
+}
